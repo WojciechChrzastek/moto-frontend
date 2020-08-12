23b7dde0535ecaf157aed1aa32ba3e13ce77fc32
Wojciech Chrzastek <w.chrzastek@gmail.com>
2020-08-12 16:57:37 +0200

    Add new class body and comment unsused code

--- src/components/user/register.js
+++ src/components/user/register.js
@@ -1,14 +1,150 @@
 import React, {Component} from 'react';
-import UserService from "../../services/user.service";
+import UserDataService from "../../services/user.service";
 
-import UserAlert from '../../alert.js';
+// import UserAlert from '../../alert.js';
 
-class Register extends Component {
+export default class Register extends Component {
 
     constructor(props) {
         super(props);
-        this.alert = React.createRef();
+        // this.alert = React.createRef();
+        this.onChangeUsername = this.onChangeUsername.bind(this);
+        this.onChangePassword = this.onChangePassword.bind(this);
+        this.onChangeEmail = this.onChangeEmail.bind(this);
+        this.saveUser = this.saveUser.bind(this);
+        this.newUser = this.newUser.bind(this);
+
+        this.state = {
+            id: null,
+            username: "",
+            password: "", 
+            email: "",
+
+            submitted: false
+          };
     }
+
+    onChangeUsername(e) {
+        this.setState({
+          username: e.target.value
+        });
+      }
+
+    onChangePassword(e) {
+        this.setState({
+          password: e.target.value
+        });
+      }
+
+      onChangeEmail(e) {
+        this.setState({
+          email: e.target.value
+        });
+      }
+
+    saveUser() {
+        var data = {
+          username: this.state.username,
+          password: this.state.password,
+          email: this.state.email
+        };
+
+        UserDataService.create(data)
+        .then(response => {
+            this.setState({
+            id: response.data.id,
+            username: response.data.username,
+            password: response.data.password,
+            email: response.data.email,
+
+            submitted: true
+            });
+            console.log(response.data);
+        })        
+        .catch(e => {
+            console.log(e);
+        });    
+    }
+
+    newUser() {
+        this.setState({
+          id: null,
+          username: "",
+          password: "",
+          email: "",
+
+          submitted: false
+        });
+      }
+
+    render() {
+        return (
+            <div className="Register">
+                {this.state.submitted ? (
+                    <div>
+                        <h4>You submitted successfully!</h4>
+                        <button
+                        className="btn btn-success" onClick={this.newUser}>
+                        Register
+                        </button>
+                    </div>
+                ) : (    
+                    <div className="Register">       
+                    <h1 className="RegisterHeader">Register</h1>
+                    
+                    <form className="submit-form" onSubmit={this.handleSubmit}>               
+                        
+                        <div className="form-group" controlid="username">
+                            <label htmlFor="username">Username</label>
+                            <input 
+                            className="form-control"
+                            name="username"
+                            id="username"
+                            type="text"
+                            placeholder="Enter username"
+                            autoFocus
+                            required
+                            value={this.state.username}
+                            onChange={this.onChangeUsername}/> 
+                        </div>
+
+                        <div className="form-group" controlid="password">
+                            <label htmlFor="password">Password</label>
+                            <input
+                            className="form-control"
+                            name="password"
+                            id="password"
+                            type="password"
+                            placeholder="Enter password"
+                            required
+                            value={this.state.password}
+                            onChange={this.onChangePassword}/>
+                        </div>
+
+                        <div className="form-group" controlid="email">
+                            <label htmlFor="email">Email</label>
+                            <input
+                            className="form-control"
+                            name="email"
+                            id="email"
+                            type="email"
+                            placeholder="Enter email"
+                            required
+                            value={this.state.email}
+                            onChange={this.onChangeEmail}/>
+                            <small className="form-text text-muted">
+                                Please provide a valid email address.
+                            </small>
+                        </div>
+
+                        <button onClick={this.saveUser} className="btn btn-primary" type="submit" size="lg">Register</button>
+                    </form>
+                    </div>
+                )}
+            </div>
+        );
+    }
+}
 
     // handleSubmit = event => {
     //     event.preventDefault();
@@ -27,19 +163,21 @@ class Register extends Component {
     //             password: password,
     //             email: email,
     //         })
-    //     }).then(function (response) {
-    //         if (response.status === 200) {
-    //             this.showAlert("success", "User registered!", "You can now log in using your credentials.");
-    //         } else if (response.status === 422) {
-    //             this.showAlert("danger", "User/email already exists", "Please choose a different name/email.");
-    //         } else if (response.status === 406) {
-    //             this.showAlert("danger", "Insufficient data.", "Please fill in all fields.");
-    //         } else {
-    //             this.showAlert("danger", "User not registered!", "Something went wrong.");
-    //         }
-    //     }.bind(this)).catch(function (error) {
-    //         this.showAlert("danger", "Error", "Something went wrong.");
-    //     }.bind(this));
+    //     })
+        // .then(function (response) {
+        //     if (response.status === 200) {
+        //         this.showAlert("success", "User registered!", "You can now log in using your credentials.");
+        //     } else if (response.status === 422) {
+        //         this.showAlert("danger", "User/email already exists", "Please choose a different name/email.");
+        //     } else if (response.status === 406) {
+        //         this.showAlert("danger", "Insufficient data.", "Please fill in all fields.");
+        //     } else {
+        //         this.showAlert("danger", "User not registered!", "Something went wrong.");
+        //     }
+        // }.bind(this)).catch(function (error) {
+        //     this.showAlert("danger", "Error", "Something went wrong.");
+        // }.bind(this));
+
     // }
 
     // showAlert(variant, heading, message) {
@@ -49,47 +187,8 @@ class Register extends Component {
     //     this.alert.current.setVisible(true);
     // }
 
-    render() {
-        return (
-            <div className="Register">
-                <h1 className="RegisterHeader">Register</h1>
-                <form className="submit-form" onSubmit={this.handleSubmit}>               
-                    <div className="form-group" controlid="username">
-                        <label>Username</label>
-                        <input 
-                         className="form-control"
-                         name="username"
-                         type="text"
-                         placeholder="Enter username"
-                         autoFocus/> 
-                    </div>
 
-                    <div className="form-group" controlid="password">
-                        <label>Password</label>
-                        <input
-                         className="form-control"
-                         name="password"
-                         type="password"
-                         placeholder="Enter password"/>
-                    </div>
 
-                    <div className="form-group" controlid="email">
-                        <label>Email</label>
-                        <input
-                         className="form-control"
-                         name="email"
-                         type="email"
-                         placeholder="Enter email"/>
-                        <small className="form-text text-muted">
-                            Please provide a valid email address.
-                        </small>
-                    </div>
-
-                    <button className="btn btn-primary" type="submit" size="lg">Register</button>
-                </form>
-                <UserAlert ref={this.alert}/>
-            </div>
-            
         //   <div className="submit-form">
         //     {this.state.submitted ? (
         //       <div>
@@ -131,9 +230,3 @@ class Register extends Component {
         //         </button>
         //       </div>
         //     )}
-          
-        );
-    }
-}
-
-export default Register;
