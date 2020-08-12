5032dea1065e1ab3b8e1d5c77a5a349b5c8aa70b
Wojciech Chrzastek <w.chrzastek@gmail.com>
2020-08-12 16:54:57 +0200

    Comment unused code

--- src/App.js
+++ src/App.js
@@ -1,9 +1,43 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+
+import Register from './components/user/register.js'
 
 class App extends Component {
   render() {
-    // ...
+    return (
+      <Router>
+        {/* <div> */}
+          <nav className="navbar navbar-expand navbar-dark bg-dark">
+            <a href="/" className="navbar-brand m-auto">MotoApp</a>
+            <div className="navbar-nav m-auto">
+              {/* <li className="nav-item">
+                <Link to={"/tutorials"} className="nav-link">
+                  Tutorials
+                </Link>
+              </li> */}
+              <a className="nav-item nav-link" href="/register">Register</a>
+            </div>
+          </nav>
+
+          {/* <div className="container mt-3">
+            <Switch>
+              <Route exact path={["/", "/tutorials"]} component={TutorialsList} />
+              <Route exact path="/add" component={AddTutorial} />
+              <Route path="/tutorials/:id" component={Tutorial} />
+            </Switch>
+          </div> */}
+
+          <div className="container">
+            <Route path="/register" component={Register}/>
+          </div>
+
+
+        {/* </div> */}
+      </Router>
+    );
   }
 }
 
