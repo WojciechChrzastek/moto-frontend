86fed19108b767c92007be506692e3c5df8baff0
Wojciech Chrzastek <w.chrzastek@gmail.com>
2020-08-19 10:38:49 +0200

    Add users-list.js

---
+++ src/components/user/users-list.js
@@ -0,0 +1,184 @@
+import React, { Component } from "react";
+import UserDataSerice from "../../services/user.service";
+import { Link } from "react-router-dom";
+
+export default class UsersList extends Component {
+  constructor(props) {
+    super(props);
+    this.onChangeSearchName = this.onChangeSearchName.bind(this);
+    this.retrieveUsers = this.retrieveUsers.bind(this);
+    this.refreshList = this.refreshList.bind(this);
+    this.setActiveUser = this.setActiveUser.bind(this);
+    this.removeAllUsers = this.removeAllUsers.bind(this);
+    this.searchUser = this.searchUser.bind(this);
+
+    this.state = {
+      users: [],
+      currentUser: null,
+      currentIndex: -1,
+      searchName: ""
+    };
+  }
+
+  componentDidMount() {
+    this.retrieveUsers();
+  }
+
+  onChangeSearchName(e) {
+    const searchTitle = e.target.value;
+
+    this.setState({
+      searchTitle: searchTitle
+    });
+  }
+
+  retrieveUsers() {
+    UserDataSerice.getAll()
+      .then(response => {
+        this.setState({
+          users: response.data
+        });
+        console.log(response.data);
+      })
+      .catch(e => {
+        console.log(e);
+      });
+  }
+
+  refreshList() {
+    this.retrieveUsers();
+    this.setState({
+      currentUser: null,
+      currentIndex: -1
+    });
+  }
+
+  setActiveUser(user, index) {
+    this.setState({
+      currentUser: user,
+      currentIndex: index
+    });
+  }
+
+  removeAllUsers() {
+    UserDataSerice.deleteAll()
+      .then(response => {
+        console.log(response.data);
+        this.refreshList();
+      })
+      .catch(e => {
+        console.log(e);
+      });
+  }
+
+  searchUser() {
+    UserDataSerice.findByTitle(this.state.searchTitle)
+      .then(response => {
+        this.setState({
+          users: response.data
+        });
+        console.log(response.data);
+      })
+      .catch(e => {
+        console.log(e);
+      });
+  }
+
+  render() {
+    const { searchTitle, users, currentUser, currentIndex } = this.state;
+
+    return (
+      <div className="list row">
+        <div className="col-md-8">
+          <div className="input-group mb-3">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Search by username"
+              value={searchTitle}
+              onChange={this.onChangeSearchName}
+            />
+            <div className="input-group-append">
+              <button
+                className="btn btn-outline-secondary"
+                type="button"
+                onClick={this.searchUser}
+              >
+                Search
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="col-md-6">
+          <h4>Users List</h4>
+
+          <ul className="list-group">
+            {users &&
+              users.map((user, index) => (
+                <li
+                  className={
+                    "list-group-item " +
+                    (index === currentIndex ? "active" : "")
+                  }
+                  onClick={() => this.setActiveUser(user, index)}
+                  key={index}
+                >
+                  {user.username}
+                </li>
+              ))}
+          </ul>
+
+          <button
+            className="m-3 btn btn-sm btn-danger"
+            onClick={this.removeAllUsers}
+          >
+            Remove All Users
+          </button>
+        </div>
+        <div className="col-md-6">
+          {currentUser ? (
+            <div>
+              <h4>User</h4>
+              <div>
+                <label>
+                  <strong>Name:</strong>
+                </label>{" "}
+                {currentUser.username}
+              </div>
+              <div>
+                <label>
+                  <strong>Email:</strong>
+                </label>{" "}
+                {currentUser.email}
+              </div>
+              <div>
+                <label>
+                  <strong>Status:</strong>
+                </label>{" "}
+                {currentUser.published ? "Published" : "Pending"}
+              </div>
+
+              <Link
+                to={"/users/" + currentUser.id}
+              >
+              <button
+            className="m-3 btn btn-sm btn-info"
+          >
+            Edit
+          </button>
+          </Link>
+
+
+
+            </div>
+          ) : (
+            <div>
+              <br />
+              <p>Please click on an User...</p>
+            </div>
+          )}
+        </div>
+      </div>
+    );
+  }
+}
