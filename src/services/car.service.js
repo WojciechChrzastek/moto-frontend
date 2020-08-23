e154b1f8dd22c2177d845bd63547c2dac0bd41c3
Wojciech Chrzastek <w.chrzastek@gmail.com>
2020-08-23 16:55:51 +0200

    Add remaining CRUD methods for car object

--- src/services/car.service.js
+++ src/services/car.service.js
@@ -1,12 +1,32 @@
 import http from "../http-common";
 
 class CarDataService {
+  getAll() {
+    return http.get("/cars");
+  }
+
+  get(id) {
+    return http.get(`/cars/${id}`);
+  }
+
   add(data) {
     return http.post("/carsnouser", data);
   }
 
-  getAll() {
-    return http.get("/cars");
+  update(id, data) {
+    return http.put(`/cars/${id}`, data);
+  }
+
+  delete(id) {
+    return http.delete(`/cars/${id}`);
+  }
+
+  deleteAll() {
+    return http.delete(`/cars`);
+  }
+
+  findByBrand(brand) {
+    return http.get(`/cars?brand=${brand}`);
   }
 
 }
