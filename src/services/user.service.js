348331f472e682b971919b1c7393eda10c0cc044
Wojciech Chrzastek <w.chrzastek@gmail.com>
2020-08-26 19:45:30 +0200

    Add crud methods to user.service

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,12 +1,36 @@
 import http from "../http-common";
 
 class UserDataService {
-  create(data) {
+  getAll() {
+    return http.get("/users");
+  }
+
+  get(id) {
+    return http.get(`/users/${id}`);
+  }
+
+  add(data) {
     return http.post("/users", data);
   }
 
-  getAll() {
-    return http.get("/users");
+  update(id, data) {
+    return http.put(`/users/${id}`, data);
+  }
+
+  delete(id) {
+    return http.delete(`/users/${id}`);
+  }
+
+  deleteAll() {
+    return http.delete(`/users`);
+  }
+
+  findByUsername(username) {
+    return http.get(`/users?username=${username}`);
+  }
+
+  findByEmail(email) {
+    return http.get(`/users?email=${email}`);
   }
 
 }
