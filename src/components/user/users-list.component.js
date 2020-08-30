b71b2f936fc6fc3d14faa58d20d1d60e0b459ee1
Wojciech Chrzastek <w.chrzastek@gmail.com>
2020-08-30 15:41:19 +0200

    Create searchBy method for search in users list by selected attribute

--- src/components/user/users-list.component.js
+++ src/components/user/users-list.component.js
@@ -8,24 +8,21 @@ export default class UsersListComponent extends Component {
     constructor(props) {
         super(props);
         this.alert = React.createRef();
-        this.onChangeSearchUsername = this.onChangeSearchUsername.bind(this);
-        this.onChangeSearchEmail = this.onChangeSearchEmail.bind(this);
-        this.onChangeSearchPassword = this.onChangeSearchPassword.bind(this);
+        this.onChangeSearchBy = this.onChangeSearchBy.bind(this);
+        this.searchBy = this.searchBy.bind(this);
+        this.handleSearchByChange = this.handleSearchByChange.bind(this);
         this.retrieveUsers = this.retrieveUsers.bind(this);
         this.refreshList = this.refreshList.bind(this);
         this.setActiveUser = this.setActiveUser.bind(this);
         this.removeAllUsers = this.removeAllUsers.bind(this);
-        this.searchUsername = this.searchUsername.bind(this);
-        this.searchEmail = this.searchEmail.bind(this);
-        this.searchPassword = this.searchPassword.bind(this);
 
         this.state = {
+            data: [],
             currentUser: null,
             currentIndex: -1,
-            searchUsername: "",
-            searchEmail: "",
-            searchPassword: "",
-            data: []
+            searchBy: "",
+            searchByPlaceholder: "Type in username",
+            foundUsers: 1
         };
     }
 
@@ -33,27 +30,11 @@ export default class UsersListComponent extends Component {
         this.retrieveUsers();
     }
 
-    onChangeSearchUsername(e) {
-        const searchUsername = e.target.value;
+    onChangeSearchBy(e) {
+        const searchBy = e.target.value;
 
         this.setState({
-            searchUsername: searchUsername
-        });
-    }
-
-    onChangeSearchEmail(e) {
-        const searchEmail = e.target.value;
-
-        this.setState({
-            searchEmail: searchEmail
-        });
-    }
-
-    onChangeSearchPassword(e) {
-        const searchPassword = e.target.value;
-
-        this.setState({
-            searchPassword: searchPassword
+            searchBy: searchBy
         });
     }
 
@@ -103,43 +84,72 @@ export default class UsersListComponent extends Component {
             )
     }
 
-    searchUsername() {
-        UserDataService.findByUsername(this.state.searchUsername)
-            .then(response => {
-                this.setState({
-                    users: response.data
+    searchBy() {
+        this.setState({foundUsers: 1})
+        this.setState({
+            currentUser: null,
+            currentIndex: -1
+        });
+        this.state.searchByPlaceholder === "Type in username"
+            ? UserDataService.findByUsername(this.state.searchBy)
+                .then(response => {
+                    this.setState({
+                        users: response.data
+                    });
+                    console.log(response.data);
+                })
+                .catch(e => {
+                    console.log(e);
+                    this.setState({foundUsers: 0})
+                    if (e.response.status === 404) {
+                        this.showAlert("danger", "Users not found!", "There is no user of given data.");
+                    } else {
+                        this.showAlert("danger", "Users not found!", "Something went wrong.");
+                    }
+                })
+            : UserDataService.findByEmail(this.state.searchBy)
+                .then(response => {
+                    this.setState({
+                        users: response.data
+                    });
+                    console.log(response.data);
+                })
+                .catch(e => {
+                    console.log(e);
+                    this.setState({foundUsers: 0})
+                    if (e.response.status === 404) {
+                        this.showAlert("danger", "Users not found!", "There is no user of given data.");
+                    } else {
+                        this.showAlert("danger", "Users not found!", "Something went wrong.");
+                    }
                 });
-                console.log(response.data);
-            })
-            .catch(e => {
-                console.log(e);
-            });
+
+        // : UserDataService.findByPassword(this.state.searchBy)
+        //     .then(response => {
+        //         this.setState({
+        //             users: response.data
+        //         });
+        //         console.log(response.data);
+        //     })
+        //     .catch(e => {
+        //         console.log(e);
+        //         this.setState({foundUsers: 0})
+        //         if (e.response.status === 404) {
+        //             this.showAlert("danger", "Users not found!", "There is no user of given data.");
+        //         } else {
+        //             this.showAlert("danger", "Users not found!", "Something went wrong.");
+        //         }
+        //     });
+
     }
 
-    searchEmail() {
-        UserDataService.findByEmail(this.state.searchEmail)
-            .then(response => {
-                this.setState({
-                    users: response.data
-                });
-                console.log(response.data);
-            })
-            .catch(e => {
-                console.log(e);
-            });
+    handleSearchByChange(event) {
+        this.setState({searchByPlaceholder: event.target.value});
     }
 
-    searchPassword() {
-        UserDataService.findByPassword(this.state.searchPassword)
-            .then(response => {
-                this.setState({
-                    users: response.data
-                });
-                console.log(response.data);
-            })
-            .catch(e => {
-                console.log(e);
-            });
+    toggleClass() {
+        const currentState = this.state.active;
+        this.setState({active: !currentState});
     }
 
     showAlert(variant, heading, message) {
@@ -151,7 +161,7 @@ export default class UsersListComponent extends Component {
     }
 
     render() {
-        const {searchUsername, searchEmail, searchPassword, users, currentUser, currentIndex} = this.state;
+        const {searchByPlaceholder, users, currentUser, currentIndex} = this.state;
 
         return (
             <div className="list row users-list">
@@ -159,24 +169,23 @@ export default class UsersListComponent extends Component {
                     <div className="input-group-prepend">
                         <span className="input-group-text" id="search-bar-text">Search by: </span>
                     </div>
-                    <select className="custom-select col-md-2" id="search-bar-selector">
-                        {/*<option selected>Choose...</option>*/}
-                        <option value="1">Username</option>
-                        {/*<option value="2">Email</option>*/}
+                    <select className="custom-select col-md-2" id="search-bar-selector"
+                            value={this.state.value} onChange={this.handleSearchByChange}>
+                        <option value="Type in username">Username</option>
+                        <option value="Type in email">Email</option>
                         {/*<option value="3">Password</option>*/}
                     </select>
                     <input
                         type="text"
                         className="form-control"
-                        placeholder="Type in username"
-                        value={searchUsername}
-                        onChange={this.onChangeSearchUsername}
+                        placeholder={searchByPlaceholder}
+                        onChange={this.onChangeSearchBy}
                     />
                     <div className="input-group-append">
                         <button
                             className="btn btn-outline-primary"
                             type="button"
-                            onClick={this.searchUsername}
+                            onClick={this.searchBy}
                         >
                             Search
                         </button>
@@ -186,83 +195,94 @@ export default class UsersListComponent extends Component {
                 <div className="col-md-8">
 
                     <h4>Users List</h4>
+                    {this.state.foundUsers === 1 ?
+                        (
+                            <div>
 
-                    <Table bordered hover>
-                        <thead>
-                        <tr>
-                            <th>Id</th>
-                            <th>Username</th>
-                            <th>Email</th>
-                            <th>Password</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        {users && users.map((user, index) => (
-                            <tr
-                                className={(index === currentIndex ? "table-info" : "")}
-                                index={index} onClick={() => this.setActiveUser(user, index)}
-                            >
-                                <td>{user.id}</td>
-                                <td>{user.username}</td>
-                                <td>{user.email}</td>
-                                <td>{user.password}</td>
-                            </tr>
-                        ))}
-                        </tbody>
-                    </Table>
+                                <Table bordered hover>
+                                    <thead>
+                                    <tr>
+                                        <th>Id</th>
+                                        <th>Username</th>
+                                        <th>Email</th>
+                                        <th>Password</th>
+                                    </tr>
+                                    </thead>
+                                    <tbody>
+                                    {users && users.map((user, index) => (
+                                        <tr key={user.id}
+                                            className={(index === currentIndex ? "table-info" : "")}
+                                            onClick={() => this.setActiveUser(user, index)}
+                                        >
+                                            <td>{user.id}</td>
+                                            <td>{user.username}</td>
+                                            <td>{user.email}</td>
+                                            <td>{user.password}</td>
+                                        </tr>
+                                    ))}
+                                    </tbody>
+                                </Table>
 
-                    <button
-                        className="btn btn-sm btn-danger rmv-all-btn"
-                        onClick={this.removeAllUsers}
-                    >
-                        Remove All Users
-                    </button>
+                                <button
+                                    className="btn btn-sm btn-danger rmv-all-btn"
+                                    onClick={this.removeAllUsers}
+                                >
+                                    Remove All Users
+                                </button>
+                            </div>
 
+                        ) : <UserAlert ref={this.alert}/>
+                    }
                     <UserAlert ref={this.alert}/>
-
                 </div>
 
-                <div className="col-md-4">
-                    {currentUser ? (
-                        <div>
-                            <h4>Selected User data</h4>
+                <div className="col-md-4 selected-user">
+                    {currentUser ?
+                        (
                             <div>
-                                <label>
-                                    <strong>Username:</strong>
-                                </label>{" "}
-                                {currentUser.username}
+                                <h4>Selected User data</h4>
+                                <div>
+                                    <label>
+                                        <strong>Username:</strong>
+                                    </label>{" "}
+                                    {currentUser.username}
+                                </div>
+                                <div>
+                                    <label>
+                                        <strong>Email:</strong>
+                                    </label>{" "}
+                                    {currentUser.email}
+                                </div>
+                                <div>
+                                    <label>
+                                        <strong>Password:</strong>
+                                    </label>{" "}
+                                    {currentUser.password}
+                                </div>
+                                <div>
+                                    <Link to={"/users/" + currentUser.id}>
+                                        <button className="btn btn-sm btn-info mr-3">
+                                            Edit
+                                        </button>
+                                    </Link>
+                                    {/*<Link to={"/users/" + currentUser.id}>*/}
+                                    {/*<button className="btn btn-sm btn-danger">*/}
+                                    {/*Delete*/}
+                                    {/*</button>*/}
+                                    {/*</Link>*/}
+                                </div>
                             </div>
-                            <div>
-                                <label>
-                                    <strong>Email:</strong>
-                                </label>{" "}
-                                {currentUser.email}
-                            </div>
-                            <div>
-                                <label>
-                                    <strong>Password:</strong>
-                                </label>{" "}
-                                {currentUser.password}
-                            </div>
-                            <div>
-                                <Link to={"/users/" + currentUser.id}>
-                                    <button className="btn btn-sm btn-info mr-3">
-                                        Edit
-                                    </button>
-                                </Link>
-                                {/*<Link to={"/users/" + currentUser.id}>*/}
-                                {/*<button className="btn btn-sm btn-danger">*/}
-                                {/*Delete*/}
-                                {/*</button>*/}
-                                {/*</Link>*/}
-                            </div>
-                        </div>
-                    ) : (
-                        <div>
-                            <br/>
-                            <p>Please click on an User...</p>
-                        </div>
-                    )}
+                        ) :
+                        (
+                            this.state.foundUsers === 1 ?
+                                <div>
+                                    <br/>
+                                    <p>Please click on an User...</p>
+                                </div>
+                                :
+                                ""
+                        )
+                    }
                 </div>
             </div>
         );
